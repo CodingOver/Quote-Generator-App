386426e7fc96779e87a00a71ab2534e2edfbdce0
Ayoub Fellat <paypal1business.over@gmail.com>
2021-03-10 22:34:21 +0100

    add loading & complete function for loading quote

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+// hide loading 
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    } 
+}
 // Get Quote From API 
 async function getQuote() {
+    loading();
     const apiUrl = 'https://api.quotable.io/random';
     try {
         const response = await fetch(apiUrl);
@@ -23,6 +37,8 @@ async function getQuote() {
             quoteText.classList.remove('long-quote');
         }
         quoteText.textContent = data.content;
+        // Stop Loader, Show Quote
+        complete();
     } catch (error) {
         console.log('Whoops, no quote', error);
     }
@@ -40,4 +56,4 @@ function tweetQuote() {
 newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
-
+getQuote();
